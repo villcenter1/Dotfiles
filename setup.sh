43e6616fa485eb24369b5d47eb3a346620df6bcf
fire3420 <fire3420@gmail.com>
2020-02-29 09:23:42 +0000

    modified tmux path

--- setup.sh
+++ setup.sh
@@ -210,7 +210,7 @@ if [ -e "$HOME/.tmux.conf" ]; then
 fi
 
 cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
-cp -a ./tmux/. "$HOME"/.tmux/
+cp -a $VIM/tmux/. "$HOME"/.tmux/
 ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
 
 # Install TPM plugins.
